42d13672e1106e8b831d945c49a4d7c421a2c8e1
JuanDMoreno04 <juan.moreno@ikno.com.co>
2024-09-03 16:08:56 -0500

    ci(backend): configuration of microservice

---
+++ src/main/java/com/microservices2/cargaimagenes/CargaimagenesApplication.java
@@ -0,0 +1,13 @@
+package com.microservices2.cargaimagenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaimagenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaimagenesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices2/cargaimagenes/CargaimagenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices2.cargaimagenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaimagenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

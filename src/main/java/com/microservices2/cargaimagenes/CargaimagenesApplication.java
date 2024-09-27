f1030b8f22155810140d8b8f745c1b7914f995c6
JuanDMoreno04 <juan.moreno@ikno.com.co>
2024-09-27 10:40:46 -0500

    ci(backend): connection service with another microservices

--- src/main/java/com/microservices2/cargaimagenes/CargaimagenesApplication.java
+++ src/main/java/com/microservices2/cargaimagenes/CargaimagenesApplication.java
@@ -2,6 +2,8 @@ package com.microservices2.cargaimagenes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CargaimagenesApplication {
@@ -9,5 +11,10 @@ public class CargaimagenesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CargaimagenesApplication.class, args);
 	}
+
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
